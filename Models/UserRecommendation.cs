767789185a083f9138377ef9eff6c44753d17a89
ncbarker <71114211+ncbarker@users.noreply.github.com>
2021-02-10 23:46:56 -0700

    Phone validation doesn't work, but rest is done

--- Models/UserRecommendation.cs
+++ Models/UserRecommendation.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace BarkerAssignment4.Models
 {
@@ -12,6 +14,10 @@ namespace BarkerAssignment4.Models
         public string RestaurantName { get; set; }
 
         public string FavoriteDish { get; set; } = "It's all tasty!";
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\+\s?)?((?<!\+.*)\(\+?\d+([\s\-\.]?\d+)?\)|\d+)([\s\-\.]?(\(\d+([\s\-\.]?\d+)?\)|\d+))*(\s?(x|ext\.?)\s?\d+)?$", ErrorMessage = "The phone number entered is not a valid phone number.")]
+        [Phone]
         public string RestaurantPhone { get; set; } = "Coming soon!";
     }
 }
